e611b6d12ceb95203b3de170d20d8fd69f9412b5
Aheri Mondal <aheri.mon@gmail.com>
2021-06-21 23:57:06 +0530

    Use of static keyword

---
+++ Java/Useof_static3.java
@@ -0,0 +1,10 @@
+//Static block
+class Lab4_4_3 {
+    static {
+        System.out.println("static block is invoked");
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Hello main");
+    }
+}
