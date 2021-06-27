53e0d4c4e8cd336dfa9a65c31db0e569b465f51e
Aheri Mondal <aheri.mon@gmail.com>
2021-06-27 22:05:53 +0530

    changes

--- Java/Useof_Static_4.java
+++ Java/Useof_Static_4.java
@@ -10,7 +10,7 @@ class Useof_Static_4 {
     }
 
     public static void main(String[] args) {
-        Lab4_4_4.Inner obj = new Lab4_4_4.Inner();
+        Useof_Static_4.Inner obj = new Useof_Static_4.Inner();
         obj.msg();
     }
 }
