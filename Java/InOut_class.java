6bb46d6c3277b1b31c9ccff720c60db04f85cec3
Aheri Mondal <aheri.mon@gmail.com>
2021-07-01 23:57:49 +0530

    upload

---
+++ Java/InOut_class.java
@@ -0,0 +1,31 @@
+/*
+ * Write a program to find area and perimeter of a circle. With outer class
+ * calculate area, with inner class calculate perimeter.
+ */
+
+import java.util.*;
+
+class Outerclass {
+
+    void ar(int r) {
+        double area = 3.14 * r * r;
+        System.out.println("Area is: " + area);
+    }
+
+    class Innerclass {
+        void pr(int r) {
+            double peri = 2 * 3.14 * r;
+            System.out.println("Perimeter is: " + peri);
+        }
+    }
+}
+
+class InOut_class {
+    public static void main(String[] args) {
+        Outerclass myOuter = new Outerclass();
+        Outerclass.Innerclass myInner = myOuter.new Innerclass();
+        System.out.println("Radius is 4 units");
+        myOuter.ar(4);
+        myInner.pr(4);
+    }
+}
