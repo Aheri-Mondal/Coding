bb635cc082741b59ae73c17b2c17f0a0bbb9188a
Aheri Mondal <aheri.mon@gmail.com>
2021-07-11 22:24:11 +0530

    this keyword 1

---
+++ Java/this_use1.java
@@ -0,0 +1,27 @@
+// Accessing instance variable using "this" keyword
+class this_Test
+{
+     int val1;
+     int val2;
+ 
+    // Parameterized constructor
+    this_Test(int val1, int val2)
+    {
+        this.val1 = val1 + val1;
+        this.val2 = val2 + val2;
+    }
+ 
+   void display()
+    {
+          System.out.println("Value 1 = " + val1 + "\nValue 2 = " + val2);
+    }
+}
+ 
+class this_use1{
+ 
+       public static void main(String[] args)
+       {
+            this_Test object = new this_Test(5,10);
+            object.display();
+       }
+}
