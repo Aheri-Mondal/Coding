a2464829d2efbc2b100f2543307e1fdb19279ddb
Aheri Mondal <aheri.mon@gmail.com>
2021-07-11 22:27:31 +0530

    this keyword 2

---
+++ Java/this_use2.java
@@ -0,0 +1,25 @@
+//"this" passed as method parameter
+class Test_method {
+    int val1;
+    int val2;
+
+    Test_method() {
+        val1 = 10;
+        val2 = 20;
+    }
+
+    void printVal(Test_method obj) {
+        System.out.println("value 1 = " + obj.val1 + "\nvalue 2 = " + obj.val2);
+    }
+
+    void get() {
+        printVal(this);
+    }
+}
+
+class this_use2 {
+    public static void main(String[] args) {
+        Test_method object = new Test_method();
+        object.get();
+    }
+}
