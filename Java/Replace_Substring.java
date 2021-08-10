69279e53ccc07fb6d6f1dd57e82231890a59676f
Aheri Mondal <aheri.mon@gmail.com>
2021-08-10 23:06:44 +0530

    Replacing Substring

---
+++ Java/Replace_Substring.java
@@ -0,0 +1,10 @@
+/*Write a program to replace substring with another one.*/
+
+public class Replace_Substring {
+    public static void main(String[] args) {
+
+        String str = "Vaccination";
+
+        System.out.println(str.replace("Vaccina", "Isola"));
+    }
+}
